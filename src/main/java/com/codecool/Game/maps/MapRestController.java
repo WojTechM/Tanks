437bb08d1011c953e0e76127b096c4bb3f34c756
WojTechM <wojciech.m1997@gmail.com>
2019-01-25 21:10:22 +0100

    MapRestController sends random map each time

--- src/main/java/com/codecool/Game/maps/MapRestController.java
+++ src/main/java/com/codecool/Game/maps/MapRestController.java
@@ -1,13 +1,23 @@
 package com.codecool.Game.maps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapRestController {
 
+    private GameMapGenerator mapGenerator;
+
+    @Autowired
+    public MapRestController(GameMapGenerator mapGenerator) {
+        this.mapGenerator = mapGenerator;
+    }
+
     @GetMapping(path = "/map")
     private String getMap() {
-        return "map";
+        GameMap map = mapGenerator.createMap(30, 16, 0.6f);
+        System.out.println(map);
+        return map.toJson();
     }
 }
