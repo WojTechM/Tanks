e64cd63399e88c5d4bde5a3b9a8dcc41d4f86b15
WojTechM <wojciech.m1997@gmail.com>
2019-01-25 20:18:56 +0100

    Init commit

---
+++ src/main/java/com/codecool/Game/GameApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
+
